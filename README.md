598d7c475f515dd43c7e341e2c19c13855b5af55
Jens Weber <weber2@fzi.de>
2021-08-10 14:36:13 +0200

    feat: Add the asynchronous client example
    Add the asynchronous client and its explaination using the boost::asio library
    
    No issue addressed

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # Networking introduction project
 This project is based on the video series of javidx9 (https://www.youtube.com/watch?v=2hNdkYInj4g&t=485s) to familiarize myself with networking in C++ and get a deeper understanding how to program networking applications on a very low level of abstraction. Also the most common issues are elaborated as well as discussed, how they could be solved, using the ```boost::asio``` library. The main takeaways are written down here in the ```README.md```.
 The difference between my project and the one of javidx9 is, that he uses a Windows setup by incooperating Visual Studio and its build system, whereas I using a CMake project as my build system and working completly under linux (Ubuntu 20.04 LTS). So lets get started. ;-)  
+
+## General Information about ```boost::asio```
++ boost::asio is a purely header based library and you can use the boost version of asio as well as the boost independent version of asio by downloading it from https://think-async.com/Asio/ 
++ asio could handle not only networking, but also a lot of other forms of input-output operations for external ressources (which is everything that is not associated with the process that the main executable of the programm) 
++ asio is very good at handly errors --> Error-Codes are thrown whenever something went wrong and they have a very descriptive form so you can get a good intention about what was gone wrong
+
+## Common mistakes in networking
+### main_client.cpp
+* Between the request from the client and the response from the server to the client, it takes time to transfer the bytes over the internet/network. So if we execute the read out for the socket immediatly after we sended the request to the server, there will be propably no or not all data available 
+    - One possible opportunity is to create a time delay between the request and the readout for the response in the code. But this is not good practice!
+    - Another possability is to block the calling thread for the socket to wait until there is data arrived. The problem with this is, that the execution of the socket readout is triggered as soon as the first packet of data is arrived, which results in an incomplete readout of the data.
+***The cental problem with blocking the execution before read out the socket is, that we do NOT know how long the transport of the takes. We do not know, how much time the response needs to complete, since the request is out of our computer as well as out of our control! Also, we do not know how much data we need to receive in order to get the complete data from the server. This problem is twofold: First, we do not know when the data transmission is finish, second, we do not know what size our data buffer needs to be!***
++ Possible solutions: 
+    - Make a resonably large data buffer for the aimed data that we expect to receive
+    - Use the asynchronous functionalities of boost::asio --> grapSomeData function in ```main_client.cpp```
+
+### main_client_async.cpp
++ In order to not block the main function from execution while waiting for the full data to be arrived, we can use the asynchronous functions of ```boost::asio```. But caution: Thinking asynchronously is not that easy! All ```async_``` functions of asio return immediatly after they were called in the code. In the background, the OS will do the asynchronous tasks that are associated with the so called "I/O objects" that are encoded by the ```boost::asio async_``` commands. They all want a handler (which is a callback function/function pointer that follows some rules) which is called by the I/O service object, when its .run() method is called. 
+    - CAUTION: The .run() method is a blocking method for the thread that is calling the ```ioserv.run()``` in the code. 
++ Good practice: We do not want the main thread to be blocked. So the ```ioserv.run()``` will be executed in its own thread, which solves the described problem
+    - Problem: After .run() was called and the I/O service object has nothing to do (i.e. all work that is defined by I/O objects like ```socket.async_read_some(...)```), the I/O service object ends its execution capability. So if we receive multiple data packages over the network over time, just the first packed will be received and the I/O service object will end. 
+    - Solution: Give the I/O service object some idle work to do so that it does not end its execution capability until we say ```ioserv.stop()```.
++ You can see the code in ```main_client_async.cpp``` especially in line 49. 
---
+++ src/main_client_async.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <chrono>
+
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/ts/buffer.hpp>
+#include <boost/asio/ts/internet.hpp>
+
+
+
+std::vector<char> global_receive_storage(1*1024); // storage of 20 Bytes 
+
+
+void readout_handle(boost::system::error_code & ec, std::size_t length){
+    if (!ec){
+        std::cout << "\n\nRead " << length << " bytes\n\n"; 
+
+        for (int i = 0; i < length; i++){
+            std::cout << global_receive_storage[i]; 
+        }
+    }
+}
+
+void grapSomeData(boost::asio::ip::tcp::socket & socket){
+    socket.async_read_some(boost::asio::buffer(global_receive_storage.data(), global_receive_storage.size()), 
+        // here comes the read handle as a lambda function, so we can avoid to define the socked globally 
+        [&](boost::system::error_code ec, std::size_t length){
+            if (!ec){
+                std::cout << "\n\nRead " << length << " bytes\n\n"; 
+                
+                for (int i = 0; i < length; i++){
+                    std::cout << global_receive_storage[i];
+                }
+                grapSomeData(socket);
+            }
+        } // end of the lambda function (i.e. head handler)
+    ); // end of async_read_some(buffer, read_handler)
+}
+
+
+int main(){
+    /*
+    * Asynchronous http request to a server using boost::asio
+    */
+
+    boost::asio::io_service ioserv; // the I/O service object lets the program end as soon as there are no I/O services (and/or associated I/O objects) registered in the I/O service object
+    
+    boost::asio::io_context::work idle_work(ioserv); // give the I/O service object some idle work (ref: CAUTION 2) 
+    // starting a thread that runs the I/O service object to run in parallel to the main thread
+    std::thread thr([&](){
+        ioserv.run(); 
+        // Explaination: https://stackoverflow.com/questions/15568100/confused-when-boostasioio-service-run-method-blocks-unblocks
+    }); 
+        // CAUTION: .run() blocks the thread that calls the .run() method of the I/O service object
+        // In order to not block the main thread by the I/O service object, we run the I/O service object in its own thread
+        // CAUTION2: As soon as the I/O service object has no I/O services registered with it, it ends its lifetime. So if we did not 
+        // associate the idle work to the I/O service object, it would end (and can not awake again!) before the actual asynchronous 
+        // async_read_some(...) function is invoced! 
+    
+
+    
+    boost::system::error_code ec; 
+    // boost::asio::ip::tcp::endpoint tcp_endpt(boost::asio::ip::make_address("93.184.216.34", ec), 80);
+    boost::asio::ip::tcp::endpoint tcp_endpt(boost::asio::ip::make_address("51.38.81.49", ec), 80); // DeepL IP address  
+    boost::asio::ip::tcp::socket sock(ioserv); 
+    
+    // connect to the endpoint
+    sock.connect(tcp_endpt, ec); 
+    if (!ec){ 
+        std::cout << "Connected - continue programm\n"; 
+    } else {
+        std::cerr << "An error occured: " << ec.message() << "\n";
+        sock.close(); 
+        return 1; 
+    }
+
+    grapSomeData(sock); // listening to the socket for new data arrived before the request to the server was started 
+
+    // sending a http request to the server
+    std::string http_request = "GET /index.html HTTP/1.1\r\n"
+                                "Host: example.com\r\n"
+                                "Connection: close\r\n\r\n";
+        // \r is a repositioning to the beginning of the current screen line
+    if (sock.is_open()){   
+        // check if the socket could connect to the defined endpoint --> we have an active and alive connection to the other end(-point) of the server
+        sock.write_some(boost::asio::buffer(http_request.data(), http_request.size()), ec); // send as much data of the given data as possible
+            // We need to use asio buffers in order to read and write with sockets. This is an interface for the asio framework
+    }  
+
+    // grapSomeData(sock); // wrong place, since we want to read something immediatly after we sended the request. 
+    // The asynchrounous functions of boost::asio are immediatly returning after they were called (since they are asynchronously) 
+    // and therefore, the main function will end before any data was arrived on the socked 
+    
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000)); // wait a resonable amount of time at the end of the program, to receive the full data 
+    // (in a real world scenario, this would be an infinite loop or something like this, so there would be no reason to wait) but we achived by incooperating
+    // some fake idle_work, that we can receive the full data and do not block the main thread
+    ioserv.stop(); // hard stop the I/O service object that is doing the idle_work after receiving the full website  
+    
+    // join the main thread to avoid an segfault at the end of the program
+    if (thr.joinable()){
+        thr.join(); 
+    }
+
+    std::cout << "Finished reading!\n";
+
+    return 0; 
+}
