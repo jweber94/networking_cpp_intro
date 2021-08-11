9e611a04ae0bb342980a3d1aebdbeb43c3758a59
Jens <jweber1094@gmail.com>
2021-08-11 19:37:19 +0200

    feat: Add test for the message datatype
    Added the test for the net_message.hpp by checking the read (<<) and
    write (>>) operators.
    
    No issue addressed

--- tests/test_serialization.cpp
+++ tests/test_serialization.cpp
@@ -4,18 +4,28 @@
 TEST(sample_test_case, sample_test)
 {
     
-    enum class my_ids {
-        entry_one, 
-        entry_two
+    enum class CustomMsgTypes : uint32_t {
+        MsgID1, 
+        MsgID2
     };
 
-    float test_payload = 5.5; 
+    float test_payload_1 = 5.5;
+    bool test_payload_2 = true; 
+    int test_payload_3 = 3;
 
-    custom_netlib::message<my_ids> test_msg;
-    test_msg.header.id = my_ids::entry_one; 
-    test_msg << test_payload; 
+    // create the message based on the test payload of different types    
+    custom_netlib::message<CustomMsgTypes> test_msg;
+    test_msg.header.id = CustomMsgTypes::MsgID1; //my_ids::entry_one; 
+    test_msg << test_payload_1 << test_payload_2 << test_payload_3; 
     
-    std::cout << "The payload is: " << test_msg << "\n"; 
+    // create variables with known wrong entrys and then save their new values based on the message
+    float test_returned_payload_1 = 1.1;
+    bool test_returned_payload_2 = false; 
+    int test_returned_payload_3 = 1;
 
-    EXPECT_EQ(1, 1);
+    test_msg >> test_returned_payload_3 >> test_returned_payload_2 >> test_returned_payload_1;  
+
+    EXPECT_EQ(test_payload_1, test_returned_payload_1);
+    EXPECT_EQ(test_payload_2, test_returned_payload_2);
+    EXPECT_EQ(test_payload_3, test_returned_payload_3);
 }
